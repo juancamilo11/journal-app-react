3c8bd59df4987cc094af9b09f203021e765eb77c
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-11-16 23:34:10 -0500

    Estilos para el RCegisterScreen terminados

--- src/routers/AuthRouter.jsx
+++ src/routers/AuthRouter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import LoginScreen from "../pages/auth/LoginScreen";
-import RegisterScren from "../pages/auth/RegisterScren";
+import { RegisterScreen } from "../pages/auth/RegisterScreen";
 
 const AuthRouter = () => {
   return (
@@ -9,7 +9,7 @@ const AuthRouter = () => {
       <div className="auth__box-container">
         <Switch>
           <Route exact path="/auth/login" component={LoginScreen} />
-          <Route exact path="/auth/register" component={RegisterScren} />
+          <Route exact path="/auth/register" component={RegisterScreen} />
           <Redirect to="/auth/login" />
         </Switch>
       </div>
