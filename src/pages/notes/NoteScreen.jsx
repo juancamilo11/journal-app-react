de80f4b070cf1c00c799d3d28142a781ebdb1e47
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-11-17 01:16:11 -0500

    HomeScreen terminado

--- src/pages/notes/NoteScreen.jsx
+++ src/pages/notes/NoteScreen.jsx
@@ -5,6 +5,26 @@ const NoteScreen = () => {
   return (
     <div className="notes__main-container">
       <NotesAppBar />
+      <div className="notes__content">
+        <input
+          type="text"
+          placeholder="Write the title here"
+          className="notes__title-input"
+          autoComplete="off"
+        />
+        <textarea
+          placeholder="What's on your mind?"
+          className="notes__textarea"
+        ></textarea>
+
+        <div className="notes__image">
+          <img
+            src="https://fotoarte.com.uy/wp-content/uploads/2019/03/Landscape-fotoarte.jpg"
+            alt="landscape"
+            style={{ width: "25%" }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
