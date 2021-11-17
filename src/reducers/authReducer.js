7b5e7c1676e2cda2b22ea771a501772e65dfab37
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-11-17 07:57:49 -0500

    Se implemetó el patrón Redux para el login

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,26 @@
+import { types } from "../types/types";
+
+/*
+ if logged:
+{
+    uid: dkasljd23453943,
+    name: Camilo
+ }
+
+ if not logged:
+ {}
+ */
+
+export const authReducer = (state = {}, action) => {
+  switch (action.type) {
+    case types.login:
+      return {
+        uid: action.payload.uid,
+        name: action.payload.displayName,
+      };
+    case types.logout:
+      return {};
+    default:
+      return state;
+  }
+};
---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { combineReducers, createStore } from "redux";
+import { authReducer } from "../reducers/authReducer";
+
+const reducers = combineReducers({
+  auth: authReducer,
+});
+
+export const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
